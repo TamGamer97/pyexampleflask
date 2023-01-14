9fa0763c9efcd78e202d9eef517b4729b07e52d2
Tamim Miah <75170424+TamGamer97@users.noreply.github.com>
2023-01-14 11:25:42 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def mainRoute():
-    return jsonify('Welcome to quran api by TamDev')
+def hello_world():
+    return 'Hello World!'
 
-@app.route('/getArabicTxt/<urlAudio>')
-def arabicTxt(urlAudio):
-   
-    # arabTxt = urlAudio
-    return urlAudio
-
-
-if(__name__ == '__main__'):
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
